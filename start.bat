bfe86423833f4b6d1e73b03b08392c5a8d7ac3ea
marcue483 <marcue483@gmail.com>
2025-06-16 21:24:03 +0300

    migrations + superuser creation at ./start

--- start.bat
+++ start.bat
@@ -5,5 +5,14 @@ docker compose -f deploy/caddy.compose.yaml up -d
 docker compose -f admin/compose.yaml up -d
 docker compose -f store/compose.yaml up -d
 
+docker compose -f store/compose.yaml exec store python manage.py migrate
+
+    set /p createadmin=Would you like to create a superuser? (y/n): 
+    IF "%createadmin%"=="y" (
+        docker compose -f store/compose.yaml exec store python manage.py createsuperuser
+    ) ELSE (
+        echo Skipping superuser creation.
+    )
+
 echo All services started successfully.
 pause
